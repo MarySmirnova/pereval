8d9070b385dd62d0f008804e6c7bc3dbf13bd9de
MarySmirnova <marysmirnova_work@mail.ru>
2022-02-23 22:55:17 +1000

    work on rest

--- internal/rest/server.go
+++ internal/rest/server.go
@@ -1,11 +1,15 @@
 package rest
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/MarySmirnova/pereval/internal/config"
+	"github.com/MarySmirnova/pereval/pkg/storage/models"
 )
 
 type Worker struct {
@@ -16,7 +20,7 @@ func NewWorker(cfg config.REST) *Worker {
 	wr := &Worker{}
 
 	handler := mux.NewRouter()
-	handler.Name("submit_data").Methods(http.MethodPost).Path("/submitData").HandlerFunc(wr.submitData)
+	handler.Name("submit_data").Methods(http.MethodPost).Path("/submitData").HandlerFunc(wr.submitDataHandler)
 
 	wr.httpServer = &http.Server{
 		Addr:         cfg.Listen,
@@ -32,6 +36,18 @@ func (wr *Worker) GetHTTPServer() *http.Server {
 	return wr.httpServer
 }
 
-func (wr *Worker) submitData(w http.ResponseWriter, r *http.Request) {
+func (wr *Worker) submitDataHandler(w http.ResponseWriter, r *http.Request) {
+	var pereval models.Pereval
 
+	err := json.NewDecoder(r.Body).Decode(&pereval)
+	if err != nil {
+		log.WithError(err).Warn("unable to parse the request") // TODO: prepare public error description
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(pereval)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 }
--- pkg/storage/models/model.go
+++ pkg/storage/models/model.go
@@ -1,1 +1,35 @@
 package models
+
+type Pereval struct {
+	ID          string  `json:"id"`
+	BeautyTitle string  `json:"beautyTitle"`
+	Title       string  `json:"title"`
+	OtherTitles string  `json:"other_titles"`
+	Connect     string  `json:"connect"`
+	AddTime     string  `json:"add_time"`
+	User        User    `json:"user"`
+	Coords      Coords  `json:"coords"`
+	Type        string  `json:"type"`
+	Level       Level   `json:"level"`
+	Images      []Image `json:"images"`
+}
+
+type User struct{}
+
+type Coords struct {
+	Latitude  string `json:"latitude"`
+	Longitude string `json:"longitude"`
+	Height    string `json:"height"`
+}
+
+type Level struct {
+	Winter string `json:"winter"`
+	Summer string `json:"summer"`
+	Autumn string `json:"autumn"`
+	Spring string `json:"spring"`
+}
+
+type Image struct {
+	URL   string `json:"url"`
+	Title string `json:"title"`
+}
