cd0bba4b417f53ef1a1d0d6ce8aea553ce74e854
Maria Smirnova <m.smirnova@vladlink.ru>
2022-03-03 09:22:07 +1000

    fix config

--- internal/config/postgres.go
+++ internal/config/postgres.go
@@ -1,9 +1,9 @@
 package config
 
 type Postgres struct {
-	Host     string `env:"DB_HOST" envDefault:"localhost"`
-	Database string `env:"DB_DATABASE" envDefault:"pereval"`
+	Host     string `env:"DB_HOST"`
+	Database string `env:"DB_DATABASE"`
 	User     string `env:"DB_USER"`
 	Password string `env:"DB_PASSWORD"`
-	Port     int    `env:"DB_PORT" envDefault:"5432"`
+	Port     int    `env:"DB_PORT"`
 }
